ffbdbf4afd5d0e037462417e4ff3fd843c71a3e0
Chris Kim <chris.kim342@gmail.com>
2016-06-21 19:32:16 -0700

    test

--- ManagementAppPractice/ViewController.swift
+++ ManagementAppPractice/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // change for initial commit
+        // test #1
     }
 
     override func didReceiveMemoryWarning() {
